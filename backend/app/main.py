9a39dddb7fc9ef456c0b49652fc9073757454691
Aalok singh <alok123reena@gmail.com>
2026-03-12 10:51:45 +0530

    fixes;

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Nexus Event AI System",
     version="1.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
